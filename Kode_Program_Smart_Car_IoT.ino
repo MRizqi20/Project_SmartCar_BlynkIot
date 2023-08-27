fc24f4e63973e85a9d1ed8a1eb4270048cbd11fe
Muhammad Rizqi Rahmatullah <134224418+MRizqi20@users.noreply.github.com>
2023-08-27 13:12:16 +0700

    Update Kode_Program_Smart_Car_IoT.ino

--- Kode_Program_Smart_Car_IoT.ino
+++ Kode_Program_Smart_Car_IoT.ino
@@ -1,79 +1,119 @@
-#define BLYNK_TEMPLATE_ID "TMPL6u2K-g_oP"
-#define BLYNK_TEMPLATE_NAME "Smart Monitoring"
-#define BLYNK_AUTH_TOKEN "DoiQ3RBVE7Nws0BCnbDJOVs3h54Z33fL"
+#define BLYNK_TEMPLATE_ID "TMPL6z1YlxH2a"
+#define BLYNK_TEMPLATE_NAME "SmartCar"
+#define BLYNK_AUTH_TOKEN "QkM1lEa9V_ZzFbC4PldUqKsx2OWBuaad"
 
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-
-char auth [] = "Token_Auth_Blynk";
-char ssid [] = "Nama Wifi";
-char pass [] = "Password";
-
 //Motor L298N pin
 #define ENA D0
 #define IN1 D1
 #define IN2 D2
 #define IN3 D3
 #define IN4 D4
-#define ENB D6
+#define ENB D5
 
-BlynkTimer timer;
+bool forward =0;
+bool backward =0;
+bool left=0;
+bool right=0;
+int Speed;
+char auth[] = BLYNK_AUTH_TOKEN ;
+char ssid[] =  "Redmi Note 10 Pro" ;
+char pass [] = "11223344" ;
 
-void setup() {
-  Serial.begin(9600);
-  Blynk.begin (auth,ssid,pass);
-  pinMode (ENA,OUTPUT);
+void setup () {
+Serial.begin(9600);
   pinMode (IN1,OUTPUT);
   pinMode (IN2,OUTPUT);
   pinMode (IN3,OUTPUT);
   pinMode (IN4,OUTPUT);
-  pinMode (ENB,OUTPUT);
+  pinMode (ENA,OUTPUT);
 
-  timer.setInterval(1000, sendSensor);
+  Blynk.begin(auth, ssid, pass);
 }
 
-void loop () {
-  Blynk.run();
-  timer.run();
-}
-
-void sendSensor() {
-
+BLYNK_WRITE(V0) {
+  forward = param.asInt();
 }
 
 BLYNK_WRITE(V1) {
-  int x = param[0].asInt();
-  int y = param[1].asInt();
+  backward = param.asInt();
+}
 
-  if (y>50) {
-    digitalWrite (IN1, HIGH);
-    digitalWrite (IN2,LOW);
-    analogWrite (ENA, map(y, 0, 255, 0, 1023));
+BLYNK_WRITE(V2) {
+  left = param.asInt();
+}
+
+BLYNK_WRITE(V3) {
+  right = param.asInt();
+}
+
+BLYNK_WRITE(V4) {
+  Speed = param.asInt();
+}
+
+void smartcar() {
+  if (forward==1) {
+carforward();
+Serial.println("carforward");
+  }else if (backward==1) {
+    carbackward();
+    Serial.println("carbackward");
+  }else if (left==1) {
+    carturnleft();
+    Serial.println("carleft");
+  }else if (right==1) {
+    carturnright();
+    Serial.println("carright");
+  }else if (forward==0 && backward ==0 && left == 0 && right ==0) {
+    carStop();
+    Serial.println("carstop");
   }
-  else if (y<-50) {
-   digitalWrite (IN1, LOW);
-   digitalWrite (IN2,HIGH);
-   analogWrite (ENA, map(-y, 0, 255, 0, 1023));
-  }
-  else {
-   digitalWrite (IN1, LOW);
-   digitalWrite (IN2,LOW);
-   analogWrite (ENA,0);
-  }
-if (x>50) {
-    digitalWrite (IN3, HIGH);
-    digitalWrite (IN4,LOW);
-    analogWrite (ENB, map(x, 0, 255, 0, 1023));
-  }
-  else if (y<-50) {
-   digitalWrite (IN3, LOW);
-   digitalWrite (IN4,HIGH);
-   analogWrite (ENB, map(-x, 0, 255, 0, 1023));
-  }
-  else {
-   digitalWrite (IN3, LOW);
-   digitalWrite (IN4,LOW);
-   analogWrite (ENB,0);
-  }
-  }
+}
+
+void loop (){
+Blynk.run();
+smartcar();
+}
+
+void carforward (){
+  analogWrite (ENA,Speed);
+  analogWrite (ENB,Speed);
+  digitalWrite (IN1,LOW);
+  digitalWrite (IN2,HIGH);
+  digitalWrite (IN3,HIGH);
+  digitalWrite (IN4,LOW);
+}
+void carbackward (){
+  analogWrite (ENA,Speed);
+  analogWrite (ENB,Speed);
+  digitalWrite (IN1,HIGH);
+  digitalWrite (IN2,LOW);
+  digitalWrite (IN3,LOW);
+  digitalWrite (IN4,HIGH);
+}
+void carturnleft (){
+  analogWrite (ENA,Speed);
+  analogWrite (ENB,Speed);
+  digitalWrite (IN1,HIGH);
+  digitalWrite (IN2,LOW);
+  digitalWrite (IN3,HIGH);
+  digitalWrite (IN4,LOW);
+}
+void carturnright (){
+  analogWrite (ENA,Speed);
+  analogWrite (ENB,Speed);
+  digitalWrite (IN1,LOW);
+  digitalWrite (IN2,HIGH);
+  digitalWrite (IN3,LOW);
+  digitalWrite (IN4,HIGH);
+}
+void carStop (){
+  analogWrite (ENA,Speed);
+  analogWrite (ENB,Speed);
+  digitalWrite (IN1,LOW);
+  digitalWrite (IN2,LOW);
+  digitalWrite (IN3,LOW);
+  digitalWrite (IN4,LOW);
+}
